22d14d28c02b664ac712a72defb2f330efcfa4a6
Wyatt Condon <wyatt.condon@yahoo.com>
2018-12-29 20:07:54 -0500

    use iis website

--- HomeWork.App/MainWindow.xaml.cs
+++ HomeWork.App/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Windows;
+﻿using System.Windows;
 
 namespace HomeWork.App
 {
@@ -14,56 +10,14 @@ namespace HomeWork.App
     {
         public MainWindow() => InitializeComponent();
 
-        private static readonly IEnumerable<Process> DotnetRunProcesses =
-            new[]
-            {
-                new Process
-                {
-                    StartInfo = new ProcessStartInfo
-                    {
-                        FileName = "dotnet",
-                        Arguments = $"{Environment.CurrentDirectory}\\website\\Web\\HomeWork.Web.dll",
-                        UseShellExecute = true,
-                        RedirectStandardOutput = false,
-                        RedirectStandardError = false,
-                        CreateNoWindow = true,
-                        WorkingDirectory = $"{Environment.CurrentDirectory}\\website\\Web\\HomeWork.Web.dll"
-                    }
-
-                },
-                new Process
-                {
-                    StartInfo = new ProcessStartInfo
-                    {
-                        FileName = "dotnet",
-                        Arguments = $"{Environment.CurrentDirectory}\\website\\Api\\HomeWork.Api.dll",
-                        UseShellExecute = true,
-                        RedirectStandardOutput = false,
-                        RedirectStandardError = false,
-                        CreateNoWindow = true,
-                        WorkingDirectory = $"{Environment.CurrentDirectory}\\website\\Api\\HomeWork.Api.dll"
-                    }
-
-                }
-            };
-
         private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
         {
-            DotnetRunProcesses.ToList().ForEach(x => x.Start());
-            const string webServerAddress = "http://localhost:44382/";
+            const string webServerAddress = "https://localhost:44382/";
             ChromiumContainer.Content = new CefSharp.Wpf.ChromiumWebBrowser()
             {
                 Address = webServerAddress
             };
-            Closed += Window_Closed;
         }
-
-        private static void Window_Closed(object sender, EventArgs e)
-        {
-            DotnetRunProcesses.ToList().ForEach(process =>
-            {
-                if (!process.HasExited) process.Kill();
-            });
-        }
+        
     }
 }
